2f2f46e55be36b643db7e20b5d95d8e8302b01be
Dao Duc Tam <ddt@gmail.com>
2025-12-07 13:06:54 +0700

    v1

--- NotificationMS/src/main/java/com/hms/NotificationMS/NotificationMsApplication.java
+++ NotificationMS/src/main/java/com/hms/NotificationMS/NotificationMsApplication.java
@@ -2,8 +2,10 @@ package com.hms.NotificationMS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationMsApplication {
 
 	public static void main(String[] args) {
